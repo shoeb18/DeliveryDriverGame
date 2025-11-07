90fecae5a93de3d352704f2def214d8045fed88e
shoeb khan <shoebkhan0197@gmail.com>
2025-11-07 10:35:20 +0530

    feat: Implement package pickup and delivery mechanics - Added functionality to CarCollision.cs for picking up packages and delivering them to customers. - Introduced color change for the car when a package is picked up. - Updated CarController.cs to include speed boost mechanics when colliding with SpeedUp objects.

--- Assets/Scripts/CarCollision.cs
+++ Assets/Scripts/CarCollision.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class CarCollision : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor;
+    bool hasPackage = false;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.gameObject.name);
@@ -9,7 +18,19 @@ public class CarCollision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (collision.CompareTag("Package") && !hasPackage)
+        {
+            Destroy(collision.gameObject, 0.35f);
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Debug.Log("Package Picked!!");
+        }
+        if (collision.CompareTag("Customer") && hasPackage)
+        {
+            hasPackage = false;
+            spriteRenderer.color = Color.white;
+            Debug.Log("Package Delivered!!");
+        }
     }
 }
 
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,22 +3,38 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed = 0.01f;
+    [SerializeField] private float normalMoveSpeed = 5f;
+    [SerializeField] private float boostMoveSpeed = 10f;
     [SerializeField] private float rotateSpeed = 0.1f;
+
+    private float currentSpeed;
 
     void Start()
     {
-        
+        currentSpeed = normalMoveSpeed;
     }
 
     void Update()
     {
         // Input
         float horizontalInput = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
-        float verticalInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
 
         // Movement
         transform.Rotate(0, 0, -horizontalInput);
-        transform.Translate(0, verticalInput,0);
+        transform.Translate(0, verticalInput, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SpeedUp"))
+        {
+            currentSpeed = boostMoveSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed = normalMoveSpeed;
     }
 }
